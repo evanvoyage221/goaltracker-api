867a6d05c008ef1769e1b161d93d7268d53ac5fd
Wenxin Yang <yang.wenxin@northeastern.edu>
2021-08-06 13:57:13 +0800

    modify node version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors")
 
-import postRoutes from './routes/posts.js';
+const postRoutes = require( './routes/posts.js');
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://admin:admin123456@cluster0.yjw9v.mongodb.net/Cluster0?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
