6e37936e400c330afc08b668948957efd1990e34
Wenxin Yang <yang.wenxin@northeastern.edu>
2021-08-06 15:50:27 +0800

    delete db url

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require("cors")
-
+const cors = require("cors");
+const dotenv = require("dotenv");
 const postRoutes = require( './routes/posts.js');
 
+
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -15,7 +17,7 @@ app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
