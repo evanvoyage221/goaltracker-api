5e91eb93248876978b42e4d4cc4be0a70b32a837
Wenxin Yang <yang.wenxin@northeastern.edu>
2021-08-06 15:45:56 +0800

    deployed api

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin123456@cluster0.yjw9v.mongodb.net/Cluster0?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
